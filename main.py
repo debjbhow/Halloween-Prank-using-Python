3b2d7ef99368434f34c5987f9d217c0af63e72c7
Debjit Bhowmik <debjitbhowmik807@gmail.com>
2020-10-31 11:11:39 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from time import sleep
+
+pygame.init()
+window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+pygame.mixer.init()
+
+pygame.mixer.music.load("ratsasan.mp3")
+pygame.mixer.music.play()
+sleep(5)
+
+pygame.mixer.music.load("scary.mp3")
+pygame.mixer.music.play()
+sleep(3)
+
+image= pygame.image.load('scr.jpg')
+window.blit(image, (0,0))
+pygame.display.update()
+sleep(5)
